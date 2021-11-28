adf5e8aff0533c681ea98a03e8e61044dccdca3c
Isaac A. White <whitis01@gmail.com>
2021-11-28 17:29:54 -0600

    Refactored the setup for Custom Events.
    The previous files were becoming bloated and
    starting to throw too many errors as we have
    not yet arrived at components as their own
    classes or file structure. Currently everything
    is simply just functional programming.

---
+++ mainEvent.ts
@@ -0,0 +1,17 @@
+Vue.component('money-coupon', {
+    template:
+    `
+      <h1>Hello World</h1>
+    `,
+    // data() {
+    //     return {
+    //         isaac: 5,
+    //     }
+    // },
+    // props: {},
+    // methods: {},
+})
+
+new Vue({
+    el: '#root',
+});
