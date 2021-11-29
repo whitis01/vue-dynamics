81a5c7fbfbf07ad062325a76622c878bfa12bf7b
Isaac A. White <whitis01@gmail.com>
2021-11-29 05:19:17 -0600

    Event handling for dummies.
    This is for how to build a simple API to handle
    any custom event across syblings, parents, and children
    components. This makes the code much more flexible,
    readable, and maintainable.

--- mainEvent.ts
+++ mainEvent.ts
@@ -1,9 +1,25 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event,data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+}
+
 Vue.component('money-coupon', {
     template:
     `
         <div id="money-input">
             <input id="money-coupon-input" placeholder="Whom do you serve?" :value="master" @blur="onMasterChosen">
             <button id="money-coupon-button">Tell Master</button>
+            <input id="money-coupon-input" placeholder="Whom do you serve?" :value="master" @blur="onMasterChosenByEvent">
+            <button id="money-coupon-event">Tell Master</button>
         </div>
     `,
     methods: {
@@ -11,14 +27,16 @@ Vue.component('money-coupon', {
             alert(this.master + ': You are now my master');
             this.$emit('tell-the-world', this.master);
         },
+        onMasterChosenByEvent() {
+            alert(this.master + ': By decree of the event, you are now my master.');
+            Event.fire('event-to-tell-the-world', this.master);
+        }
     },
     data() {
         return {
             master: 'Saruman',
         }
     },
-    // props: {},
-    // methods: {},
 });
 
 new Vue({
@@ -37,4 +55,10 @@ new Vue({
             this.masterTold = false;
         }
     },
+    created() {
+        Event.listen('event-to-tell-the-world', (master) => {
+            alert('I am applying, I AM applying: ' + master);
+            this.masterTold = true;
+        });
+    }
 });
