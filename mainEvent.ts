0dcafd9437690b0e70988531ddb79885f21243c9
Isaac A. White <whitis01@gmail.com>
2021-11-28 18:02:34 -0600

    Fun with Custom Events and terrible masters.
    Simple example to educate myself.

--- mainEvent.ts
+++ mainEvent.ts
@@ -1,17 +1,40 @@
 Vue.component('money-coupon', {
     template:
     `
-      <h1>Hello World</h1>
+        <div id="money-input">
+            <input id="money-coupon-input" placeholder="Whom do you serve?" :value="master" @blur="onMasterChosen">
+            <button id="money-coupon-button">Tell Master</button>
+        </div>
     `,
-    // data() {
-    //     return {
-    //         isaac: 5,
-    //     }
-    // },
+    methods: {
+        onMasterChosen() {
+            alert(this.master + ': You are now my master');
+            this.$emit('tell-the-world', this.master);
+        },
+    },
+    data() {
+        return {
+            master: 'Saruman',
+        }
+    },
     // props: {},
     // methods: {},
-})
+});
 
 new Vue({
     el: '#root',
+    data() {
+        return {
+            masterTold: false,
+        }
+    },
+    methods: {
+        onCouponApplied(master) {
+            alert('I am applying, I AM applying: ' + master);
+            this.masterTold = true;
+        },
+        tellAnotherMaster() {
+            this.masterTold = false;
+        }
+    },
 });
